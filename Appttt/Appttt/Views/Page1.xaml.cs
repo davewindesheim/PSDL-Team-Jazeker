b5a94df446f8d9279acbc20b89863c5028e045c6
davewindesheim <82883312+davewindesheim@users.noreply.github.com>
2021-11-28 14:55:58 +0100

    textboxes and button for api post added
    textboxes and button for api post added

---
+++ Appttt/Appttt/Views/Page1.xaml.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Appttt.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Page1 : ContentPage
+    {
+        public Page1()
+        {
+            InitializeComponent();
+            user_1.Text = "Participant number";
+            user_2.Text = "Route";
+            user_3.Text = "Distance";
+            user_4.Text = "Time";
+        }
+    }
+}
