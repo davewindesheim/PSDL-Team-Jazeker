6778e20b1516190fe272fbfe080c9a3892788316
Delano <Novasigma89@gmail.com>
2021-12-16 13:20:57 +0100

    Geupdate versie

--- Appttt/Appttt/Views/Page1.xaml.cs
+++ Appttt/Appttt/Views/Page1.xaml.cs
@@ -11,6 +11,7 @@ using Newtonsoft.Json;
 using System.Net;
 using System.Net.Http.Headers;
 using System.Diagnostics;
+using Microsoft.AspNetCore.Authorization;
 
 namespace Appttt.Views
 {
@@ -23,38 +24,40 @@ namespace Appttt.Views
 			InitializeComponent();
 		}
 
+		[Authorize]
 		private async void BtnSend_Clicked(object sender, EventArgs e)
 		{
-			API mem = new API
+			APIPayload mem = new APIPayload
 			{
-				jahomaSmartIdStr = txtParticipant.Text,
-				contestIdStr = txtRoute.Text,
+				jahomaSmartIdStr = "2853325", // txtParticipant.Text,
+				contestIdStr = "60", //txtRoute.Text,
 				activityDateStr = txtDate.Date,
 				activityStarttimeStr = txtStartTime,
 				activityStoptimeStr = txtStopTime,
 				activityDistanceM = txtDistance.Text,
-				activityDuraionS = "10472",
-				activityMaxspeedMs = "41,5",
+				activityDurationS = "20472",
+				activityMaxspeedMs = "412",
 				activityDescriptionStr = "dinsdag middag in Flevoland",
-				activityGpxIdStr = "10472010|1623222398310|163223445510",
+				activityGpxIdStr = "10472025|1623222398325|163223445525",
 				activityStartDatetime = txtStartTime,
 				activityStopDateTime = txtStopTime,
 				activityType = "TYPE_Bike",
 				externalsourceIdStr = "JAZEKER",
-				externalSourceeVersionStr = "v0.1i-07Feb2021-1",
+				externalSourceVersionStr = "v0.1i-07Feb2021-1",
 			};
 
-			Uri RequestUri = new Uri("https://jve.jahoma.nl/nl/api/app_upload_activity/appactadd");
+			// Uri RequestUri = new Uri("https://jve.jahoma.nl/nl/api/app_upload_activity/appactadd");
+			Uri RequestUri = new Uri("https://jve.jahoma.nl/nl/api/app_upload_activity/appgetconfig");
 
 			var client = new HttpClient();
+			client.DefaultRequestHeaders.Add("x-auth-token", "KustwachtScheveningen");
 			var json = JsonConvert.SerializeObject(mem);
 			var contentJson = new StringContent(json, Encoding.UTF8, "application/json");
-			var Accesstoken = "X-AUTH-TOKEN KustwachtScheveningen";
-			client.DefaultRequestHeaders.Add("Authorization", "Api-key " + Accesstoken);
-			var response = await client.PostAsync(RequestUri, contentJson);
+			var response = await client.PostAsync( RequestUri, contentJson);
+			Console.WriteLine(response);
 			if (response.StatusCode == HttpStatusCode.OK)
 			{
-				await DisplayAlert("Data", "Succes. Verzending succesvol.", "Ok");
+				await DisplayAlert("Data", "Succes. Verzending succesvol." + "content:" + json + " end", "Ok");
 			}
 
 			else if (response.StatusCode == HttpStatusCode.Unauthorized)
@@ -63,15 +66,14 @@ namespace Appttt.Views
 			}
 
 			else if (response.StatusCode == HttpStatusCode.MethodNotAllowed)
-			{
+            {
 				await DisplayAlert("Data", "Error. Method not allowed.", "Ok");
-			}
+            }
 
 			else
-			{
+            {
 				await DisplayAlert("Data", "Error. Versturing defaald", "Ok");
-			}
-			Debug.WriteLine(client.DefaultRequestHeaders.Authorization);
+            }
 		}
 	}
 };
