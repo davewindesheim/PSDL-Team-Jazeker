bd4951092023475a7fd17bab3d405bd96480248d
davewindesheim <82883312+davewindesheim@users.noreply.github.com>
2021-12-20 19:22:55 +0100

    updated homepage
    moved old homepage and propertiespage to buttons on the new homepage.
    
    Co-Authored-By: FrankGIT-1 <82883472+FrankGIT-1@users.noreply.github.com>

---
+++ Appttt/Appttt/Views/Profile.xaml.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Appttt.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Profile : ContentPage
+    {
+        public Profile()
+        {
+            InitializeComponent();
+        }
+
+        private async void TapGestureRecognizer_Tapped_1(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new AboutPage());
+        }
+
+        private async void TapGestureRecognizer_Tapped_2(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new PropertiesPage());
+        }
+    }
+}
