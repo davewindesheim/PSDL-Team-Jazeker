ca0e9dbf6c579bd6cfa2f9e6a916beb090a2f168
davewindesheim <82883312+davewindesheim@users.noreply.github.com>
2021-12-21 00:12:33 +0100

    added leaderboards support & multiple buttons
    Leaderboards can now be found on the homepage including buttons for settings, qr scan, camera and info.
    
    Co-Authored-By: FrankGIT-1 <82883472+FrankGIT-1@users.noreply.github.com>

---
+++ Appttt/Appttt/Models/Leaderboards.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Appttt.Models
+
+{
+    public class Leaderboards
+    {
+        public int ItemID { get; set; }
+        public string ItemName { get; set; }
+        public string Url { get; set; }
+    }
+}
---
+++ Appttt/Appttt/ViewModels/PickerViewModel.cs
@@ -0,0 +1,38 @@
+﻿using Appttt.Models;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Text;
+
+namespace Appttt.ViewModels
+{
+    public class PickerViewModel: INotifyPropertyChanged
+    {
+        public List<Leaderboards> Leaderboard { get; set; }
+        public PickerViewModel()
+        {
+            Leaderboard = new List<Leaderboards>();
+            Leaderboard.Add(new Leaderboards() { ItemID = 1, ItemName = "The 1000 of Lier 2021/2022", Url = "https://jve.jahoma.nl/nl/contest/15/" });
+            Leaderboard.Add(new Leaderboards() { ItemID = 2, ItemName = "The 100 of Lier 2021/2022", Url = "https://jve.jahoma.nl/nl/contest/16/" });
+            Leaderboard.Add(new Leaderboards() { ItemID = 3, ItemName = "Virtual Tourist Windesheim", Url = "https://jve.jahoma.nl/nl/contest/60/" });
+        }
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        void OnPropertyChanged([CallerMemberName] string propertyName = "")
+        {
+            if (PropertyChanged == null)
+                return;
+
+            PropertyChanged.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+        public Leaderboards _Leaderboards;
+        public Leaderboards SelectedLeaderboards
+        {
+            get { return _Leaderboards; }
+            set { _Leaderboards = value;
+                OnPropertyChanged();
+            }
+        }
+    }
+}
