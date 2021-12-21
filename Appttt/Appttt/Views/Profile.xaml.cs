05c4c004e599ba4568fca456cccb5796a2500883
davewindesheim <82883312+davewindesheim@users.noreply.github.com>
2021-12-21 12:34:07 +0100

    moved camera and added new camera button
    moved camera and added new camera button
    
    Co-Authored-By: FrankGIT-1 <82883472+FrankGIT-1@users.noreply.github.com>

--- Appttt/Appttt/Views/Profile.xaml.cs
+++ Appttt/Appttt/Views/Profile.xaml.cs
@@ -1,12 +1,14 @@
 ﻿using Appttt.Models;
+using System.ComponentModel;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+using Xamarin.Essentials;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
+using ZXing.Net.Mobile.Forms;
 
 namespace Appttt.Views
 {
@@ -26,6 +28,42 @@ namespace Appttt.Views
         private async void TapGestureRecognizer_Tapped_2(object sender, EventArgs e)
         {
             await Navigation.PushAsync(new PropertiesPage());
+        }
+        // Button to open the current QR scanner, it awaits for user to show an QR when succesfully scan, it returns "result"
+        // the results currently shows in an DisplayAlert which should be updated later on.
+        private async void TapGestureRecognizer_Tapped_4(object sender, EventArgs e)
+        {
+            var scan = new ZXingScannerPage();
+            await Navigation.PushModalAsync(scan);
+
+            scan.OnScanResult += (result) =>
+            {
+                Device.BeginInvokeOnMainThread(async () =>
+                {
+                    await Navigation.PopModalAsync();
+                    await DisplayAlert("Scanned", "" + result.Text, "Ok");
+                });
+            };
+        }
+        private async void TapGestureRecognizer_Tapped_3(object sender, EventArgs e)
+        {
+            Plugin.Media.Abstractions.MediaFile photo = await Plugin.Media.CrossMedia.Current.TakePhotoAsync(new Plugin.Media.Abstractions.StoreCameraMediaOptions() { });
+            if (photo != null)
+            {
+                PhotoImage.Source = ImageSource.FromStream(() => { return photo.GetStream(); });
+            }
+
+            PermissionStatus permissions = await Permissions.CheckStatusAsync<Permissions.Camera>();
+
+            if (permissions != PermissionStatus.Granted)
+            {
+                permissions = await Permissions.RequestAsync<Permissions.Camera>();
+            }
+
+            if (permissions != PermissionStatus.Granted)
+            {
+                return;
+            }
         }
     }
 }
--- Appttt/Appttt/Views/PropertiesPage.xaml.cs
+++ Appttt/Appttt/Views/PropertiesPage.xaml.cs
@@ -20,28 +20,6 @@ namespace Appttt.Views
         {
             InitializeComponent();
         }
-        // When camera button is clicked the code below awaits the camera and checks for permission,
-        // if not granted yet it asks for permission.
-        private async void ShowCamera_Clicked(object sender, EventArgs e)
-        {
-            Plugin.Media.Abstractions.MediaFile photo = await Plugin.Media.CrossMedia.Current.TakePhotoAsync(new Plugin.Media.Abstractions.StoreCameraMediaOptions() { });
-            if (photo != null)
-            {
-                PhotoImage.Source = ImageSource.FromStream(() => { return photo.GetStream(); });
-            }
-
-            PermissionStatus permissions = await Permissions.CheckStatusAsync<Permissions.Camera>();
-
-            if (permissions != PermissionStatus.Granted)
-            {
-                permissions = await Permissions.RequestAsync<Permissions.Camera>();
-            }
-
-            if (permissions != PermissionStatus.Granted)
-            {
-                return;
-            }
-        }
         // Button to get current Location, it currently returns the Latitude and Longitude in text, the task.delay
         // should be in the documentation of Geolocation.
         async void Button_Clicked(System.Object sender, System.EventArgs e)
@@ -69,23 +47,6 @@ namespace Appttt.Views
         {
             isGettingLocation = false;
 
-        }
-        // Button to open the current QR scanner, it awaits for user to show an QR when succesfully scan, it returns "result"
-        // the results currently shows in an DisplayAlert which should be updated later on.
-        private async void Button_ClickedScan(object sender, EventArgs e)
-        {
-            var scan = new ZXingScannerPage();
-            await Navigation.PushModalAsync(scan);
-
-            scan.OnScanResult += (result) =>
-            {
-                Device.BeginInvokeOnMainThread(async () =>
-                {
-                    await Navigation.PopModalAsync();
-                    await DisplayAlert("Scanned", "" + result.Text, "Ok");
-
-                });
-            };
         }
     }
 }
