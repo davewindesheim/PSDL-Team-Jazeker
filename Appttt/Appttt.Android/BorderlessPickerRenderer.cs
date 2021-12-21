49bc331d2e42f95f6f86b5b014cecd6b36546a7a
davewindesheim <82883312+davewindesheim@users.noreply.github.com>
2021-12-21 15:43:43 +0100

    added custompicker and removed underline name
    added custompicker and removed underline name
    
    Co-Authored-By: FrankGIT-1 <82883472+FrankGIT-1@users.noreply.github.com>

---
+++ Appttt/Appttt.Android/BorderlessPickerRenderer.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Appttt;
+using Appttt.Droid.Renderers;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.Android;
+
+[assembly: ExportRenderer(typeof(BorderlessPicker), typeof(BorderlessPickerRenderer))]
+namespace Appttt.Droid.Renderers
+{
+    public class BorderlessPickerRenderer : Xamarin.Forms.Platform.Android.AppCompat.PickerRenderer
+    {
+        public BorderlessPickerRenderer(Context context) : base(context)
+        {
+        }
+
+        protected override void OnElementChanged(ElementChangedEventArgs<Picker> e)
+        {
+            base.OnElementChanged(e);
+
+            if (e.OldElement == null)
+            {
+                Control.Background = null;
+            }
+        }
+    }
+}
