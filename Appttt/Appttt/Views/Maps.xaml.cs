ea1c69f08981071fd1d8be80873aaf20a3fe6035
davewindesheim <82883312+davewindesheim@users.noreply.github.com>
2022-01-10 11:32:22 +0100

    including img for release
    including img for release

--- Appttt/Appttt/Views/Maps.xaml.cs
+++ Appttt/Appttt/Views/Maps.xaml.cs
@@ -37,22 +37,8 @@ namespace Appttt.Views
                     Console.WriteLine($"Latitude: {location.Latitude}, Longitude: {location.Longitude}, Altitude: {location.Altitude}");
                 }
             }
-            catch (FeatureNotSupportedException fnsEx)
-            {
-                // Handle not supported on device exception
-            }
-            catch (FeatureNotEnabledException fneEx)
-            {
-                // Handle not enabled on device exception
-            }
-            catch (PermissionException pEx)
-            {
-                // Handle permission exception
-            }
             catch (Exception ex)
-            {
-                // Unable to get location
-            }
+            { Console.WriteLine(ex.Message); }
         }
     }
 }
